a08d565de5a351449058443804b670a649993eb7
Bruce Devlin <DevlinGamers@gmail.com>
2023-06-28 12:55:02 +0100

    Updated Unity Engine version and finished off player controller.
    -Added Gravity
    -Added SimTime
    -Tidied up
    -Added setup for a weapon class
    -Added placeholder sprites for guns to test with
    -Made sandbox scene alittle more useful (added some ramps/obstacles)

---
+++ 2iGame/Assets/#Core/Scripts/Code/Player/PlayerWeapon.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeapon
+{
+    public static bool melee {get; set;}
+    public static GameObject model {get; set;}
+    public static GameObject projectile {get; set;}
+    public static float ammo {get; set;}
+    public static float damage {get; set;}
+    public static float rateOfFire {get; set;}
+}
